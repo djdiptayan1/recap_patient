d0b9f19a2de35126c0866d288389cf87056031d6
Diptayan Jash <111650788+djdiptayan1@users.noreply.github.com>
2024-11-20 10:58:48 +0530

    ✨ initial commit

--- recap/App/SceneDelegate.swift
+++ recap/App/SceneDelegate.swift
@@ -13,10 +13,27 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = TabbarViewController()
+        window.makeKeyAndVisible()
+        self.window = window
+        
+//        let questionItem = UIApplicationShortcutItem(
+//            type: "daily-questions",
+//            localizedTitle: "Daily Questions",
+//            localizedSubtitle: "Answer today's questions",
+//            icon: UIApplicationShortcutIcon(systemImageName: "questionmark.circle.fill")
+//        )
+//        let familyItem = UIApplicationShortcutItem(
+//            type: "family-members",
+//            localizedTitle: "Family Members",
+//            localizedSubtitle: "View family members",
+//            icon: UIApplicationShortcutIcon(systemImageName: "person.3.fill")
+//            )
+//        
+//        
+//        UIApplication.shared.shortcutItems = [questionItem, familyItem]
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
